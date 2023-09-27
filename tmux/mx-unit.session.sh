35e8ee901ffd77a99407123df7ffbd051eb40ad5
Laurentiu Ciobanu <laurentiu@ciobanu.dev>
2023-09-27 12:09:55 +0000

    feat: improve tmuxifier layout

--- tmux/mx-unit.session.sh
+++ tmux/mx-unit.session.sh
@@ -8,7 +8,6 @@ if initialize_session "mx-unit"; then
   select_pane 1
 
   new_window "localnet"
-  run_cmd "cd localnet"
   run_cmd "clear"
 
   new_window "chain"
@@ -30,6 +29,21 @@ if initialize_session "mx-unit"; then
 
   select_pane 1
   # run_cmd "nvim ."
+
+
+  new_window "services"
+  run_cmd "cd mx-api-service"
+  run_cmd "clear"
+
+  split_v 66
+  run_cmd "cd mx-delegation-service"
+  run_cmd "clear"
+
+  split_v 50
+  run_cmd "cd mx-socket-api-mock"
+  run_cmd "clear"
+
+  select_pane 1
 
   select_window 1
 fi
