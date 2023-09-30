9fca6189a9dd8d744c2098a1f0488a32bc2dd47d
Laurentiu Ciobanu <laurentiu@ciobanu.dev>
2023-09-30 22:15:15 +0000

    fix: cleanup tmuxifier session

--- tmux/mx-unit.session.sh
+++ tmux/mx-unit.session.sh
@@ -20,17 +20,6 @@ if initialize_session "mx-unit"; then
   select_pane 1
   run_cmd "nvim ."
 
-  new_window "vm"
-  # run_cmd "cd mx-chain-go"
-
-  split_v 25
-  # run_cmd "cd mx-chain-go"
-  # run_cmd "clear"
-
-  select_pane 1
-  # run_cmd "nvim ."
-
-
   new_window "services"
   run_cmd "cd mx-api-service"
   run_cmd "clear"
